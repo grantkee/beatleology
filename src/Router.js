d355aaee546f57ee1f5ffc9c70e8e5575ca553fb
grantkee <grant.d.kee@gmail.com>
2020-04-09 17:30:59 -0500

    Added signin and signup basic layouts from material-ui.

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Main from './components/Main';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
-const Router = () => {
+export default function Router() {
   return (
     <Switch>
       <Route exact path='/' component={Main} />
     </Switch>
   )
-}
-
-export default Router;
+};
